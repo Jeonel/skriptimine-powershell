0fba85e88fa338175d283a5803f167b3d875c551
Jeonel Fortuna-Juks <jeonel.fortuna-juks@voco.ee>
2023-11-23 15:36:12 +0200

    Tegin kasutaja kustutamise skripti

---
+++ Ylesanne_2.ps1
@@ -0,0 +1,14 @@
+﻿
+$Eesnimi = Read-Host "Sisesta oma eesnimi"
+$Perenimi = Read-Host "Sisesta oma perekonnanimi"
+
+$Kasutajanimi = "$Eesnimi.$Perenimi"
+
+$existingUser = Get-LocalUser -Name $Kasutajanimi -ErrorAction SilentlyContinue
+
+if ($existingUser) {
+    Remove-LocalUser -Name $Kasutajanimi -Verbose
+    Write-Host "Kasutaja '$Kasutajanimi'  on kustutatud."
+} else {
+    Write-Host "Kasutajat '$Kasutajanimi' ei leitud."
+}
