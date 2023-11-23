c1c7f7bcdea2f7f2f1c0f6dfe57600778b8ed03a
Jeonel Fortuna-Juks <jeonel.fortuna-juks@voco.ee>
2023-11-23 15:19:04 +0200

    Tegin lokaalse kasutaja loomise

---
+++ Ylesanne_1.ps1
@@ -0,0 +1,22 @@
+﻿
+$Eesnimi = Read-Host "Sisesta oma eesnimi"
+$Perenimi = Read-Host "Sisesta oma perekonnanimi"
+
+$Kasutajanimi = "$Eesnimi.$Perenimi"
+
+if(Get-LocalUser -Name $Kasutajanimi -ErrorAction SilentlyContinue) {
+    Write-Host "Viga: Kasutaja '$Kasutajanimi' on juba olemas."
+
+} else {
+
+$Parool = Read-Host "Sisesta parool" -AsSecureString
+
+$Kontokirjeldus = Read-host "Konto kirjeldus"
+
+New-LocalUser $Kasutajanimi -Password $Parool -FullName "$Eesnimi $Perenimi" -Description "$Kontokirjeldus"
+
+Echo "Kasutaja loodud:"
+Echo "Kasutajanimi: $Kasutajanimi"
+Echo "Täisnimi: $Eesnimi $Perenimi"
+Echo "Konto kirjeldus: $Kontokirjeldus"
+}
